607bd65301aa308f853a1914c6e91d410198644e
fabturing <fabien.carbo@protonmail.com>
2026-05-08 19:49:29 +0200

    ajout tonton v2

--- agents/TontonFlop.js
+++ agents/TontonFlop.js
@@ -1,9 +1,8 @@
-class TontonFlop extends PercAgent { //✏️ Le nom de la classe doit être le nom de l'agent en en PascalCase
-				//✏️ Remplacer `extends Agent` par `extends MelodicAgent` ou PercAgent ou BassAgent
+class TontonFlop extends PercAgent {
   constructor(){
-    super("Tonton flop", //✏️ Le nom qui s'affiche
-    "Tape dans ses mains et tape du pied par terre. Musicalement c'est pas un génie mais il a un don pour mettre l'ambiance.", //✏️ Description de l'agent qui s'affiche'
-    "tontonflop"); //✏️ L'identifiant de l'agent (nom de l'agent en lowercase)
+    super("Tonton flop",
+    "Tape dans ses mains et tape du pied par terre. Musicalement c'est pas un génie mais il a un don pour mettre l'ambiance.",
+    "tontonflop");
     this.anim = new Anim('default', true);
 
   // moods
@@ -17,11 +16,11 @@ class TontonFlop extends PercAgent { //✏️ Le nom de la classe doit être le 
 
   async loadInstrument(){
     const samples = {C3: "kick.mp3",
-                    C4: "clap.mp3"} //✏️
-    await this.loadSampler(samples, "samples/tonton/"); //✏️
+                    C4: "clap.mp3"}
+    await this.loadSampler(samples, "samples/tonton/");
 
-    this.setPan(PAN_TONTON); //✏️ Définir une constante spécifique dans js/settings.js
-    this.setVolume(VOL_TONTON); //✏️ Définir une constante spécifique dans js/settings.js
+    this.setPan(PAN_TONTON);
+    this.setVolume(VOL_TONTON);
   }
 
   generateStructure(){
