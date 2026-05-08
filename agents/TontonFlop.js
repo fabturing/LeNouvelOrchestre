6f5de9d5c8cfb599447674da560d73e441e37a7c
fabturing <fabien.carbo@protonmail.com>
2026-05-08 20:45:01 +0200

    le push du bug

--- agents/TontonFlop.js
+++ agents/TontonFlop.js
@@ -2,16 +2,18 @@ class TontonFlop extends PercAgent {
   constructor(){
     super("Tonton flop",
     "Tape dans ses mains et tape du pied par terre. Musicalement c'est pas un génie mais il a un don pour mettre l'ambiance.",
-    "tontonflop");
+    "tontonflop",['kick', 'clap']);
     this.anim = new Anim('default', true);
 
   // moods
-    this.addMood('main1', 30);
+    this.addMood('kick1', 30);
     this.addMood('mix1', 50);
-    this.addMood('pied1', 30);
-     this.addMood('main2', 30);
+    this.addMood('clap1', 30);
+    /*
+     this.addMood('kick2', 30);
     this.addMood('mix2', 50);
-    this.addMood('pied2', 30);
+    this.addMood('clap2', 30);
+    */
   }
 
   async loadInstrument(){
@@ -24,21 +26,38 @@ class TontonFlop extends PercAgent {
   }
 
   generateStructure(){
-    return ['A','B','C','B'];
+    return ['B','A','A','B'];
   }
 
-  generateNotesPattern(){
-	  return Pattern.newFromRepeatedUniform(this.scale);
-  }
 
-  generatePlaysPattern(){
-    let pattern = [100, 0, 100, 0, 100, 0, 100, 0];
-	return Pattern.newFromPercents(pattern);
+ generatePlaysPattern(line){
+    let pattern = [100, 100, 100, 100, 100, 100, 100, 100];
+
+    return Pattern.newFromPercents(pattern);
   }
 
   generatePart(partName, line){
 	let part = super.generatePart(partName, line);
+	let pattern;
+    if(this.moodIs('mix1')){
+	  if(line=='clap')      pattern =[[0,1], [1,0], [1,1], [0,1], [1,1], [0,1], [1,0], [1,1]];
+      else if(line=='kick')  pattern = [[1,0], [0,1], [0,0], [1,0], [0,0], [1,0], [0,1], [0,0]];
+    }
+    else if(this.moodIs('clap1')){
+	  if(line=='clap')      pattern =[[0,1], [1,0], [1,1], [0,1], [1,1], [0,1], [1,0], [1,1]];
+      else if(line=='kick')  pattern = [[0],[0],[0],[0],[0],[0],[0],[0]];
+    }
+    else if(this.moodIs('kick1')){
+	  if(line=='clap')      pattern = [[0],[0],[0],[0],[0],[0],[0],[0]];
+      else if(line=='kick')  pattern = [[1,0], [0,1], [0,0], [1,0], [0,0], [1,0], [0,1], [0,0]];
+    }
+
+    pattern =[[0,1], [1,0], [1,1], [0,1], [1,1], [0,1], [1,0], [1,1]];
+	part.setAttribute('rythms', pattern)
 	return part;
   }
 }
+
+
+
 
